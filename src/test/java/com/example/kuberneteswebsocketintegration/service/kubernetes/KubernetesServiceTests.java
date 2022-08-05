0378a3a6a70a84de86633bd6df7867d9e495ee33
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-08-05 23:32:52 +0200

    feature: improved tests

--- src/test/java/com/example/kuberneteswebsocketintegration/service/kubernetes/KubernetesServiceTests.java
+++ src/test/java/com/example/kuberneteswebsocketintegration/service/kubernetes/KubernetesServiceTests.java
@@ -1,10 +1,29 @@
 package com.example.kuberneteswebsocketintegration.service.kubernetes;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.k3s.K3sContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
-@ActiveProfiles({"test"})
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class KubernetesServiceTests {
-    
+    @Container
+    public static K3sContainer k3s = new K3sContainer(DockerImageName.parse("rancher/k3s:v1.21.3-k3s1"))
+            .withLogConsumer(new Slf4jLogConsumer(log));
+
+    public void testGetAllPods(){
+
+    }
+    public void testGetAllServices(){}
+    public void testGetAllNodes(){}
 }
