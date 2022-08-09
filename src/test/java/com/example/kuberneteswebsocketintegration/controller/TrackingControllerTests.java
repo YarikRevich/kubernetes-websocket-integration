1c86083aa7b25287d0bb2ab75099d21bb24ad49b
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-08-09 19:26:10 +0200

    feature: added more tests for controller

--- src/test/java/com/example/kuberneteswebsocketintegration/controller/TrackingControllerTests.java
+++ src/test/java/com/example/kuberneteswebsocketintegration/controller/TrackingControllerTests.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import java.io.StringReader;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -18,7 +16,6 @@ import javax.websocket.WebSocketContainer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.simp.stomp.StompHeaders;
@@ -37,16 +34,14 @@ import org.testcontainers.k3s.K3sContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import com.example.kuberneteswebsocketintegration.entity.ResponseEntity;
-import com.example.kuberneteswebsocketintegration.service.kubernetes.KubernetesService;
 import com.example.kuberneteswebsocketintegration.util.converter.Converters;
-import com.example.kuberneteswebsocketintegration.util.formater.ResponseFormatter;
 import com.example.kuberneteswebsocketintegration.util.server.TestServer;
 import com.example.kuberneteswebsocketintegration.util.topic.Topics;
-import com.google.gson.Gson;
 
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.models.V1NodeList;
+import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.openapi.models.V1ServiceList;
 import io.kubernetes.client.util.Config;
 import lombok.extern.slf4j.Slf4j;
@@ -62,9 +57,6 @@ public class TrackingControllerTests {
     @Container
     public static K3sContainer k3s = new K3sContainer(DockerImageName.parse("rancher/k3s:v1.21.3-k3s1"))
             .withLogConsumer(new Slf4jLogConsumer(log));
-
-    @Autowired
-    private KubernetesService kubernetesService;
 
     private WebSocketStompClient webSocketStompClient;
 
@@ -88,103 +80,6 @@ public class TrackingControllerTests {
         webSocketStompClient.setMessageConverter(Converters.getTestGsonMessageConverter());
     }
 
-    // @Test
-    // void testSendPods() throws Exception {
-    // BlockingQueue<ResponseEntity<V1PodList>> blockingQueue = new
-    // ArrayBlockingQueue<>(100);
-
-    // webSocketStompClient
-    // .connect(TestServer.getServerEndpoint(port), new StompSessionHandlerAdapter()
-    // {
-    // @Override
-    // public void afterConnected(StompSession session, StompHeaders
-    // connectedHeaders) {
-    // log.info("Connection established!");
-
-    // session.subscribe(Topics.POD, new StompSessionHandlerAdapter() {
-    // @Override
-    // public Type getPayloadType(StompHeaders headers) {
-    // return ResponseEntity.class;
-    // }
-
-    // @Override
-    // public void handleFrame(StompHeaders headers, Object payload) {
-    // System.out.println("RECEIVED!");
-    // blockingQueue.add((ResponseEntity<V1PodList>) payload);
-    // }
-    // });
-
-    // session.send("/app/pod", "");
-
-    // await()
-    // .until(() -> {
-    // return blockingQueue.size() > 0;
-    // });
-
-    // // .untilAsserted(() -> {
-    // ResponseEntity<V1PodList> expectedResponse = ResponseFormater
-    // .formatResponseFromPodList(kubernetesService.getAllPods());
-    // ResponseEntity<V1PodList> receivedResponse = blockingQueue.poll();
-
-    // System.out.println(expectedResponse.getKind());
-    // System.out.println(receivedResponse.getKind());
-
-    // // assertEquals(expectedResponse.getKind(),
-    // // receivedResponse.getKind());
-    // // assertEquals(expectedResponse.getContent().getItems().size(),
-    // // receivedResponse.getContent().getItems().size());
-    // // })
-    // }
-    // })
-    // .get(1, SECONDS);
-    // }
-
-    // @Test
-    // void testSendServices() throws Exception {
-    // BlockingQueue<ResponseEntity<V1ServiceList>> blockingQueue = new
-    // ArrayBlockingQueue<>(100);
-
-    // webSocketStompClient
-    // .connect(TestServer.getServerEndpoint(port), new StompSessionHandlerAdapter()
-    // {
-    // @Override
-    // public void afterConnected(StompSession session, StompHeaders
-    // connectedHeaders) {
-    // log.info("Connection established!");
-
-    // session.subscribe(Topics.SERVICE, new StompSessionHandlerAdapter() {
-    // @Override
-    // public Type getPayloadType(StompHeaders headers) {
-    // return ResponseEntity.class;
-    // }
-
-    // @Override
-    // public void handleFrame(StompHeaders headers, Object payload) {
-    // System.out.println("RECEIVED!");
-    // blockingQueue.add((ResponseEntity<V1ServiceList>) payload);
-    // }
-    // });
-
-    // session.send("/app/service", "");
-
-    // await()
-    // .until(() -> {
-    // return blockingQueue.size() > 0;
-    // });
-
-    // // .untilAsserted(() -> {
-    // ResponseEntity<V1ServiceList> expectedResponse = ResponseFormatter
-    // .formatResponseFromServiceList(kubernetesService.getAllServices());
-    // ResponseEntity<V1ServiceList> receivedResponse = blockingQueue.poll();
-
-    // System.out.println(expectedResponse.getKind());
-    // System.out.println(receivedResponse.getKind());
-
-    // }
-    // })
-    // .get(1, SECONDS);
-    // }
-
     @Test
     void testSendNodes() throws Exception {
         BlockingQueue<ResponseEntity<V1NodeList>> blockingQueue = new ArrayBlockingQueue<>(100);
@@ -203,7 +98,7 @@ public class TrackingControllerTests {
             @Override
             @SuppressWarnings("unchecked")
             public void handleFrame(StompHeaders headers, Object payload) {
-                blockingQueue.add((ResponseEntity<V1NodeList>) payload);
+                blockingQueue.add((ResponseEntity<V1NodeList>)payload);
             }
         });
 
@@ -214,20 +109,79 @@ public class TrackingControllerTests {
                     return blockingQueue.size() > 0;
                 });
 
-        ResponseEntity<V1NodeList> expectedResponse = ResponseFormatter
-                .formatResponseFromNodeList(kubernetesService.getAllNodes());
         ResponseEntity<V1NodeList> receivedResponse = blockingQueue.poll();
 
-        Gson gson = new Gson();
-
-        // gson.fromJson(json, typeOfT)
-                
-        // Type typeMyType = new TypeToken<ArrayList<V1Node>>(){}.getType();
-
-        System.out.println(receivedResponse.getContent().getKind());
-        assertEquals(expectedResponse.getKind(),
+        assertEquals(ResponseEntity.Kind.NodeList,
                 receivedResponse.getKind());
-        // assertEquals(Arrays.asList(expectedResponse.getContent().).size(),
-        //         Arrays.asList(receivedResponse.getContent()).size());
+    }
+
+    @Test
+    void testSendPods() throws Exception {
+        BlockingQueue<ResponseEntity<V1PodList>> blockingQueue = new ArrayBlockingQueue<>(100);
+
+        StompSession session = webSocketStompClient
+                .connect(TestServer.getServerEndpoint(port), new StompSessionHandlerAdapter() {
+                })
+                .get(1, SECONDS);
+
+        session.subscribe(Topics.POD, new StompSessionHandlerAdapter() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return ResponseEntity.class;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((ResponseEntity<V1PodList>)payload);
+            }
+        });
+
+        session.send("/app/pod", "");
+
+        await()
+                .until(() -> {
+                    return blockingQueue.size() > 0;
+                });
+
+        ResponseEntity<V1PodList> receivedResponse = blockingQueue.poll();
+
+        assertEquals(ResponseEntity.Kind.PodList,
+                receivedResponse.getKind());
+    }
+
+    @Test
+    void testSendServices() throws Exception {
+        BlockingQueue<ResponseEntity<V1ServiceList>> blockingQueue = new ArrayBlockingQueue<>(100);
+
+        StompSession session = webSocketStompClient
+                .connect(TestServer.getServerEndpoint(port), new StompSessionHandlerAdapter() {
+                })
+                .get(1, SECONDS);
+
+        session.subscribe(Topics.SERVICE, new StompSessionHandlerAdapter() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return ResponseEntity.class;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((ResponseEntity<V1ServiceList>)payload);
+            }
+        });
+
+        session.send("/app/service", "");
+
+        await()
+                .until(() -> {
+                    return blockingQueue.size() > 0;
+                });
+
+        ResponseEntity<V1ServiceList> receivedResponse = blockingQueue.poll();
+
+        assertEquals(ResponseEntity.Kind.ServiceList,
+                receivedResponse.getKind());
     }
 }
